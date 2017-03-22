14089346cb15cd9e4433d0078afd14b01966de0d
Bhrt Kmr <BhrtKmr1432@gmail.com>
2017-03-23 00:36:01 +0530

    Imported The Provided Assets

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿/*
+Copyright (c) Mr BlckHrtzz
+Let The Mind Dominate The Hrtzz
+*/
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+
+	#region Variables 
+	
+	#endregion
+	
+	#region Unity Functions
+
+	void Start () 
+	{
+		
+	}
+	
+	void Update () 
+	{
+		
+	}
+
+	#endregion
+
+	#region UserDefined
+
+	#endregion
+
+}
